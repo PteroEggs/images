ec42fe056925c7d93fc8f0255993a6d4c6eb95fe
hwalker928 <freelance@harrydev.me>
2021-05-16 17:18:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-echo "Starting Minecraft Server..."
+echo "Starting Discord Bot..."
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 eval ${MODIFIED_STARTUP}
