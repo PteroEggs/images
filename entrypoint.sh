e0c631ccd6b8f88df4b19d632335aad09ce413bd
hwalker928 <freelance@harrydev.me>
2021-05-16 19:01:33 +0100

    Testing startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 echo "Starting Minecraft Server..."
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-eval ${MODIFIED_STARTUP}
+eval "java -Xms128M -Xmx{{SERVER_MEMORY}}M -jar {{SERVER_JARFILE}}"
