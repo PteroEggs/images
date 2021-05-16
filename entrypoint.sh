62efbac26311f15a7bc993dc76aba2c11b3ecab8
hwalker928 <freelance@harrydev.me>
2021-05-16 17:05:38 +0100

    Removed startup command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 cd /home/container
-
-# Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/harrydev$ ${MODIFIED_STARTUP}"
-
-# Run the Server
+echo "Starting Minecraft Server..."
 eval ${MODIFIED_STARTUP}
