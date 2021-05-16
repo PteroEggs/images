3bae0abac616f21ac501d966e9628a8d4a9399ce
hwalker928 <freelance@harrydev.me>
2021-05-16 17:08:21 +0100

    Fixed startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 echo "Starting Minecraft Server..."
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 eval ${MODIFIED_STARTUP}
