c433c6f4d53aac8e276a343a09884a2df3ddfe9e
hwalker928 <freelance@harrydev.me>
2021-06-26 20:34:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 echo "Starting Minecraft Server..."
 SERVER_MEMORY=$(expr $SERVER_MEMORY - 1024)
+echo "Server starting with ${SERVER_MEMORY}M"
 eval "java -Xms128M -Xmx${SERVER_MEMORY}M -jar ${SERVER_JARFILE}"
